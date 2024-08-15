e39fd6d2ede08588e05f650492f1e37058bab5a7
Rosiele Sousa <rosielesousa49@gmail.com>
2024-08-15 15:38:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = window.prompt("o primeiro nome")
+let sobrenome = window.prompt("o sobrenome")
+let cursodabasetécnica = window.prompt("o curso da base técnica ")
+let anodenascimento = window.prompt("o ano de nascimento ")
+let idade = 2024 - anodenascimento
+  window.alert ("Olá" + " " + nome + " " + sobrenome + " " + "então você estuda " + cursodabasetécnica + " " + "e tem" + " " + idade +  " " + "anos" + " " + "que legal!")
