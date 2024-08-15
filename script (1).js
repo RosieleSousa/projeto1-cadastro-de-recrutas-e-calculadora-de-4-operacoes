1500da97f1e751e3812acb6d30668ddee45e4499
Rosiele Sousa <rosielesousa49@gmail.com>
2024-08-15 15:42:31 -0300

    Ex002

---
+++ script (1).js
@@ -0,0 +1,12 @@
+let número = window.prompt("digite um número")
+let número2 = window.prompt("digite outro número ")
+let resultado = parseInt(número) + parseInt(número2)
+window.alert(resultado)
+let resultado2 = parseInt(número) - parseInt(número2)
+window.alert(resultado2)
+let resultado3 = parseInt(número) * parseInt(número2)
+window.alert(resultado3)
+let resultado4 = parseInt(número) / parseInt(número2)
+window.alert(resultado4)
+                                            
+                                            
